55334ea807cb9ca8b83009e2f56e2252cc2953eb
Juani <juan.veltri@mercadolibre.com>
2020-11-19 15:00:10 -0300

    add mp button modal

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
 var bodyParser = require('body-parser');
+const mercadopago = require ('mercadopago');
+
+
+mercadopago.configure({
+  access_token: 'ACCESS_TOKEN'
+});
 
 var app = express();
  
@@ -23,8 +29,31 @@ app.get('/', function (req, res) {
 
 
 app.post("/detail", (req, res) => {
-      // Crea un objeto de preferencia
-    res.render('detail');
+  let preference = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.transactionAmount),
+        quantity: Number(req.body.quantity),
+      }
+    ]
+  };
+
+  mercadopago.preferences.create(preference)
+    .then(function(response){
+      res.render('detail', {
+        post: {
+          description: preference.items[0].title,
+          quantity: preference.items[0].quantity,
+          transactionAmount: preference.items[0].unit_price * preference.items[0].quantity,
+          id: response.body.id,
+          init_point: response.body.init_point,
+        }
+      });
+    }).catch(function(error){
+      console.log(error);
+    });
+    
   });
 
 app.listen(port);
